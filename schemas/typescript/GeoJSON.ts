6e91885b51464f38ccf64e92dca04bece78a3209
Jacob Bandes-Storch <jacob@foxglove.dev>
2022-05-20 12:59:00 -0700

    Trimmed the branch to only the ros_foxglove_msgs sub directory

--- schemas/typescript/GeoJSON.ts
+++
@@ -1,7 +0,0 @@
-// Generated from GeoJSON by @foxglove/schemas
-
-/** GeoJSON data for annotating maps */
-export type GeoJSON = {
-  /** GeoJSON data encoded as a UTF-8 string */
-  geojson: string;
-};
