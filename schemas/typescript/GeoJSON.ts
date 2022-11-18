45d52727fb6af3c36f685f9b5f56961090dee6b1
Jacob Bandes-Storch <jacob@foxglove.dev>
2022-11-18 10:58:32 -0800

    Revert "Trimmed the branch to only the ros_foxglove_msgs sub directory"
    This reverts commit 61f8a7cf7ee58fec70f973d80a37b17ad074fc36.

---
+++ schemas/typescript/GeoJSON.ts
@@ -0,0 +1,7 @@
+// Generated from GeoJSON by @foxglove/schemas
+
+/** GeoJSON data for annotating maps */
+export type GeoJSON = {
+  /** GeoJSON data encoded as a UTF-8 string */
+  geojson: string;
+};
