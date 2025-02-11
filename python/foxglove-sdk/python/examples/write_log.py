5eeaca3a2cf1e1eca3964c7b13aca9f706f50141
Jacob Bandes-Storch <jacob@foxglove.dev>
2025-02-10 18:10:23 -0800

    Trimmed the branch to only the ros_foxglove_msgs sub directory

--- python/foxglove-sdk/python/examples/write_log.py
+++
@@ -1,9 +0,0 @@
-import argparse
-import foxglove
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--path", type=str, default="output.mcap")
-args = parser.parse_args()
-
-# Open a new mcap file for recording
-foxglove.record_file(args.path)
